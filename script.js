bbed1cef838c79876ec2337ed6b8e432b2e86d0e
waka9393 <108017303+waka9393@users.noreply.github.com>
2023-10-30 13:51:19 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+// JavaScript
+
+
+
+const items = document.querySelectorAll('.img-item');
+console.log(items);
+console.log(items.length);
+
+// for文
+for(let i = 0; i < items.length; i++){
+
+    const keyframes = {
+      opacity: [0, 1],
+      translate: ['0 50px', 0],
+      filter: ['blur(10px)', 'blur(0)'],
+    };
+    
+    const options = {
+      duration: 600,
+      fill: 'forwards',
+      delay: i * 400,
+    };
+  
+    items[i].animate(keyframes, options);
+  }
